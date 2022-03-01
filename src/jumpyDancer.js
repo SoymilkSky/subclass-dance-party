187297d7199104ba15ee2c96f72be617da8ed6d9
DannyWong <danny.96.wong@gmail.com>
2022-02-28 19:38:38 -0800

    managed to get all 3 dancers to appear on the page and have 2 of them have animations so far

--- src/jumpyDancer.js
+++ src/jumpyDancer.js
@@ -1,6 +1,9 @@
 var jumpyDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
   this.timeBetweenSteps = 1000;
+  this.$node = $('<span class="jumpy"></span');
+  this.step();
+  this.setPosition(top, left);
 };
 
 jumpyDancer.prototype = Object.create(MakeDancer.prototype);
@@ -11,7 +14,7 @@ jumpyDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
 
   this.$node.animate( {bottom: '50px', top: '100px'} ); // different $node.blank( {css} );
-  this.$node.animate( {bottom: '100px', top: '50'} );
+  this.$node.animate( {bottom: '100px', top: '50px'} );
 };
 
 // {transform: rotate(45deg), background-color: pink; }
--- src/spinnyDancer.js
+++ src/spinnyDancer.js
@@ -1,5 +1,8 @@
 var makeSpinnyDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="spinny"></span');
+  this.step();
+  this.setPosition(top, left);
 };
 
 makeSpinnyDancer.prototype = Object.create(MakeDancer.prototype);
@@ -9,6 +12,7 @@ makeSpinnyDancer.prototype.constructor = makeSpinnyDancer;
 makeSpinnyDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
 
+  //this.$node.toggle();
   this.$node.animate({transform: 'translateY(-100px) rotate(1rad) scaleX(2) skewY(42deg)'});
   this.$node.animate({transform: 'translateY(-200px) rotate(1rad) scaleX(2) skewY(42deg)'});
 
